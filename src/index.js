d0734ee6c6431961bc78708a85665c1093b4c592
Eti Adam <etadam44@gmail.com>
2020-05-30 16:00:26 +0300

    Hope this will work

--- src/index.js
+++ src/index.js
@@ -15,3 +15,7 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
