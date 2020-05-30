37429b99595886686f7078cf06a5b121f37943b8
Eti Adam <etadam44@gmail.com>
2020-05-30 16:42:25 +0300

    Hope this will work

--- src/index.js
+++ src/index.js
@@ -16,5 +16,5 @@ ReactDOM.render(
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-const port = process.env.PORT || 3000;
-app.listen(port);
+// const port = process.env.PORT || 3000;
+// app.listen(port);
